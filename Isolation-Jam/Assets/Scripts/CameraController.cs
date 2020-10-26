9328282f1169495be8b609b225b960c249431e3f
GishaTheDeveloper <spaceshhz@gmail.com>
2020-10-26 21:39:39 +0200

    Add CameraController.cs & PlayerController.cs

---
+++ Isolation-Jam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region PUBLIC_FIELDS
+    [Header("Movement")]
+    public Transform target;
+    public float maxXOffset, maxZOffset;
+    public float smoothness = 1f;
+    #endregion
+
+    #region PRIVATE_FIELDS
+    Vector3 offset;
+    #endregion
+
+    #region METHODS
+
+    void Update()
+    {
+        // Камера следует за игроком и оффсетим в сторону мыши.
+        offset.x = Mathf.Lerp(offset.x, GetMouseDistFromCenter().x * maxXOffset, Time.deltaTime * smoothness);
+        offset.y = 0f;
+        offset.z = Mathf.Lerp(offset.z, GetMouseDistFromCenter().y * maxZOffset, Time.deltaTime * smoothness);
+
+        transform.position = target.position + offset;
+    }
+
+
+    Vector2 GetMouseDistFromCenter()
+    {
+        float x = Mathf.Clamp(((Input.mousePosition.x - (Screen.width / 2f)) / Screen.width) * 2f, -1f, 1f);
+        float y = Mathf.Clamp(((Input.mousePosition.y - (Screen.height / 2f)) / Screen.height) * 2f, -1f, 1f);
+        return new Vector2(x, y);
+    }
+
+    #endregion
+}
---
+++ Isolation-Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region PUBLIC_FIELDS
+    [Header("Movements")]
+    public float moveSpeed = 15f;
+    public float accFactor = 1f;
+    #endregion
+
+    #region PRIVATE_FIELDS
+    Vector3 moveInput;
+    #endregion
+
+    #region COMPONENTS
+    Rigidbody rb;
+    #endregion
+
+    #region METHODS
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        moveInput = GetMovementInput();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveInput * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    #region Movement
+    Vector3 GetMovementInput()
+    {
+        float h = Mathf.Clamp(Input.GetAxis("Horizontal") * accFactor, -1f, 1f);
+        float v = Mathf.Clamp(Input.GetAxis("Vertical") * accFactor, -1f, 1f);
+
+        return new Vector3(h, 0f, v).normalized;
+    }
+    #endregion
+
+    #endregion
+}
